ef4653fd64e19284a3e2f7d9b22843f3225134a2
eliohn <yi4534@qq.com>
2025-09-27 20:51:49 +0800

    delete(thrift): remove obsolete thrift test files
    - Deleted multiple thrift test files that are no longer needed, including test_form_query_scenarios.thrift, test_form_query.thrift, test_golang_http_tags.thrift, test_naming_styles.thrift, test_openapi_namespace.thrift, test_openapi_naming.thrift, and test_typescript_naming.thrift.
    - Added a new test file test/typescript/test_expreq.thrift to define new request structures for TypeScript.

--- test_typescript_naming.thrift
+++
@@ -1,9 +0,0 @@
-namespace typescript test_naming
-
-struct TestStruct {
-    1: string page_resp;
-    2: optional i32 page_size;
-    3: required string user_name;
-    4: optional bool is_active;
-    5: optional string user_id;
-}
